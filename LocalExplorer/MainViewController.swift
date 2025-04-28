d299dbf4cc13d98c6bb2bc29eff90580b6d84e4e
JoeNicasio123 <joenicasio123@gmail.com>
2025-04-28 16:56:12 -0500

    segment control now hides fields in view mode, and allows editing in edit mode

--- LocalExplorer/MainViewController.swift
+++ LocalExplorer/MainViewController.swift
@@ -9,13 +9,43 @@ import UIKit
 
 class MainViewController: UIViewController {
 
+    @IBOutlet weak var takePicButton: UIButton!
+    @IBOutlet weak var txtZip: UITextField!
+    @IBOutlet weak var txtState: UITextField!
+    @IBOutlet weak var txtCity: UITextField!
+    @IBOutlet weak var txtAddress: UITextField!
+    @IBOutlet weak var txtName: UITextField!
+    @IBOutlet weak var sgmtEditMode: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        self.sgmtEditMode(self)
     }
     
-
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    @IBAction func sgmtEditMode(_ sender: Any) {
+        let textFields: [UITextField] = [txtName, txtAddress, txtCity, txtState, txtZip]
+        if sgmtEditMode.selectedSegmentIndex == 0 {
+            for textField in textFields {
+                textField.isEnabled = false
+                textField.borderStyle = UITextField.BorderStyle.none
+            }
+            takePicButton.isHidden = true
+        }
+        else if sgmtEditMode.selectedSegmentIndex == 1 {
+            for textField in textFields {
+                textField.isEnabled = true
+                textField.borderStyle = UITextField.BorderStyle.roundedRect
+            }
+            takePicButton.isHidden = false
+        }
+    }
+    
     /*
     // MARK: - Navigation
 
